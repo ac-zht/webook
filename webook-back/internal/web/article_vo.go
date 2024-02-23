a1e44ff481d050d1b713ef464b9006a1b8dfe054
zht <2032754457@qq.com>
2024-02-23 18:13:16 +0800

    v1-21

---
+++ webook-back/internal/web/article_vo.go
@@ -0,0 +1,7 @@
+package web
+
+type ArticleReq struct {
+	Id      int64  `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
---
+++ webook-back/pkg/logger/nop_log.go
@@ -0,0 +1,20 @@
+package logger
+
+type NoOpLogger struct {
+}
+
+func NewNoOpLogger() Logger {
+	return &NoOpLogger{}
+}
+
+func (n *NoOpLogger) Debug(msg string, args ...Field) {}
+
+func (n *NoOpLogger) Info(msg string, args ...Field) {}
+
+func (n *NoOpLogger) Warn(msg string, args ...Field) {}
+
+func (n *NoOpLogger) Error(msg string, args ...Field) {}
+
+func (n *NoOpLogger) With(args ...Field) Logger {
+	return n
+}
